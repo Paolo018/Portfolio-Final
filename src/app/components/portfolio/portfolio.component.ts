c18b1c32a4a4db852c609cf81c9daf786f36a583
Pablo <thepablodead18@gmail.com>
2023-03-25 11:10:34 -0300

    portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit{
+  proyecto:any;
+
+  constructor(private datos:DatosService) {}
+
+  ngOnInit(): void {
+    this.datos.getDatos().subscribe(data => {
+      this.proyecto = data.proyecto;
+    })
+  }
 
 }
